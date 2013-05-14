20fa43880520bdb8b3a47e1d88856893920c374d
Senko Rasic <senko.rasic@dobarkod.hr>
2013-05-14 19:03:22 +0200

    User: add create, delete, undelete and update methods

--- hipchat/users.py
+++ hipchat/users.py
@@ -17,7 +17,7 @@ class User(HipchatObject):
 
     """
     attributes = ('user_id', 'name', 'mention_name', 'email', 'title',
-        'photo_url', 'status', 'status_message')
+        'photo_url', 'status', 'status_message', 'password')
 
     def __repr__(self):
         return '<User id=%s name="%s">' % (str(self.user_id), str(self))
@@ -60,3 +60,78 @@ class User(HipchatObject):
         """
         data = cls.api._get('users/show', user_id=str(user_id))
         return cls._parse(data['user'])
+
+    @staticmethod
+    def _maybe_dict(**kwargs):
+        return {k: v for k, v in kwargs.iteritems() if v is not None}
+
+    @classmethod
+    def create(cls, email, name, mention_name=None, title=None,
+            is_group_admin=False, password=None, timezone='UTC'):
+        """Create a new user.
+
+        Creates a new user with the provided `email` and full `name`.
+
+        If `password` is not provided, the response will contain the newly
+        created user's randomly generated password.
+
+        The user's `mention_name` specifies what will trigger mention
+        notification. If not set, it defaults to the full name. The optional
+        `title` (is set) specifies the user's title.
+
+        If `is_group_admin` is True, the newly created user will be added
+        to group admins.
+
+        The optional `timezone` argument (string) sets the user's timezone.
+        If not set, defaults to 'UTC'.
+
+        Returns a `User` object representing the newly created user. If the
+        password wasn't provided in the request, the initial password for
+        the user is returned in the `password` attribute. It should be recorded
+        (or changed) immediately as this is the only time it will be returned.
+
+        """
+        params = cls._maybe_dict(email=email, name=name, timezone=timezone,
+            is_group_admin='1' if is_group_admin else '0',
+            mention_name=mention_name, title=title, password=password)
+        data = cls.api._post('users/create', **params)
+        return cls._parse(data['user'])
+
+    def delete(self):
+        """Delete the user.
+
+        Deleted users are immediately disconnected from the chat.
+
+        """
+        self.api._post('users/delete', user_id=self.user_id)
+
+    @classmethod
+    def undelete(self, user_id=None, user_email=None):
+        """Undelete a user.
+
+        Undelete the user specified by either `user_id` (user's ID in the
+        system) or `user_email` (their e-mail).
+
+        The user will be sent a confirmation link via email.
+
+        """
+        if user_id is not None and user_email is not None:
+            raise ValueError('only one of user_id or user_email should '
+                'be provided, not both')
+
+        if user_id is None and user_email is None:
+            raise ValueError('either user_id or user_email should be provided')
+
+        self.api._post('users/undelete',
+            user_id=user_id if user_id is not None else user_email)
+
+    def update(self, email=None, name=None, mention_name=None, title=None,
+            is_group_admin=None, password=None, timezone=None):
+        """Update user information.
+
+        """
+        params = self._maybe_dict(email=email, name=name, mention_name=None,
+            is_group_admin=str(int(bool(is_group_admin)))
+            if is_group_admin is not None else None,
+            password=password, timezone=timezone)
+        self.api._post('users/update', user_id=self.user_id, **params)
